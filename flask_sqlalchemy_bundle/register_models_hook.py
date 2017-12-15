eab5defc63529e41c4a291d5960303750c1df3b7
Brian Cappello <briancappello@gmail.com>
2017-12-15 13:19:11 -0500

    configure migrations to be stored in app.config.PROJECT_ROOT/db/migrations

--- flask_sqlalchemy_bundle/register_models_hook.py
+++ flask_sqlalchemy_bundle/register_models_hook.py
@@ -2,8 +2,7 @@ import inspect
 import os
 
 from flask import Flask
-from flask_application_factory import Bundle, FactoryHook
-from typing import List
+from flask_application_factory import FactoryHook
 
 from .sqla.model import BaseModel
 
@@ -11,23 +10,14 @@ from .sqla.model import BaseModel
 class RegisterModelsHook(FactoryHook):
     priority = 25
     bundle_module_name = 'models'
+    models = {}
 
-    def __init__(self):
-        self.models = {}
-
-    def run_hook(self, app: Flask, app_config_cls, bundles: List[Bundle]):
-        bundles_with_models = []
-        for bundle in bundles:
-            models = self.collect_from_bundle(bundle)
-            if not models:
-                continue
-
-            bundles_with_models.append(bundle)
-            for name, model_class in models:
-                self.models[name] = model_class
+    def process_objects(self, app: Flask, app_config_cls, objects):
+        for name, model_class in objects:
+            self.models[name] = model_class
 
         setattr(app, 'models', self.models)
-        self.configure_migrations(app, bundles_with_models)
+        self.configure_migrations(app)
 
     def type_check(self, obj):
         if not inspect.isclass(obj):
@@ -37,16 +27,11 @@ class RegisterModelsHook(FactoryHook):
     def register_shell_context(self, ctx: dict):
         ctx.update(self.models)
 
-    def configure_migrations(self, app, bundles_with_models):
+    def configure_migrations(self, app):
         alembic = app.config.get('ALEMBIC', {})
 
-        if not alembic.get('script_location', None):
+        if not alembic.get('script_location'):
             alembic['script_location'] = os.path.join(
-                app.config['PROJECT_ROOT'], 'migrations')
-
-        if not alembic.get('version_locations', None):
-            alembic['version_locations'] = [
-                (bundle.name, os.path.join(bundle.root_dir, 'migrations'))
-                for bundle in bundles_with_models]
+                app.config['PROJECT_ROOT'], 'db', 'migrations')
 
         app.config.from_mapping({'ALEMBIC': alembic})
