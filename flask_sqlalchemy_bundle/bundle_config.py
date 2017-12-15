46bc11cd696dc768abfc985b42cdad32ae23c0b8
Brian Cappello <briancappello@gmail.com>
2017-12-15 13:19:52 -0500

    automatically import flask_sqlalchemy_bundle when needed in generated migration scripts

--- flask_sqlalchemy_bundle/bundle_config.py
+++ flask_sqlalchemy_bundle/bundle_config.py
@@ -1,7 +1,18 @@
+# import flask_sqlalchemy_bundle into generated migrations when needed
+# http://alembic.zzzcomputing.com/en/latest/autogenerate.html#affecting-the-rendering-of-types-themselves
+def render_migration_item(type_, obj, autogen_context):
+    if 'flask_sqlalchemy_bundle' in obj.__module__:
+        autogen_context.imports.add('import flask_sqlalchemy_bundle')
+    return False
+
+
 class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite://'  # :memory:
 
+    ALEMBIC_CONTEXT = {
+        'render_item': render_migration_item,
+    }
 
 class DevConfig(BaseConfig):
     pass
