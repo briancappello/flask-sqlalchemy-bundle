8150896787907ef0001839b5a6ef303edccb9b6c
Brian Cappello <briancappello@gmail.com>
2018-07-16 16:35:03 -0600

    release 0.3.1

--- flask_sqlalchemy_bundle/__init__.py
+++ flask_sqlalchemy_bundle/__init__.py
@@ -8,7 +8,7 @@
     :license: MIT, see LICENSE for more details
 """
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 
 from flask_unchained import Bundle
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='Flask SQLAlchemy Bundle',
-    version='0.3.0',
+    version='0.3.1',
     description='Adds SQLAlchemy and Alembic to Flask Unchained',
     long_description=long_description,
     url='https://github.com/briancappello/flask-sqlalchemy-bundle',
